64eff692ecb845d38d9943cfbd024d8c032bfca8
Renaud Allard <renaud@allard.it>
2026-05-17 16:57:28 +0200

    Bump version to 0.1.7

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -42,8 +42,8 @@ android {
         applicationId = "it.allard.simcountry"
         minSdk = 33
         targetSdk = 35
-        versionCode = 7
-        versionName = "0.1.6"
+        versionCode = 8
+        versionName = "0.1.7"
     }
 
     signingConfigs {
