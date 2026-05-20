4b22fc2d29e665e093da074de368a4bc6112c935
Renaud Allard <renaud@allard.it>
2026-05-20 13:54:11 +0200

    Bump version to 0.2.2

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -43,8 +43,8 @@ android {
         applicationId = "it.allard.simcountry"
         minSdk = 33
         targetSdk = 35
-        versionCode = 10
-        versionName = "0.2.1"
+        versionCode = 11
+        versionName = "0.2.2"
         ndk {
             abiFilters += "arm64-v8a"
         }
